2c68435f49878551e521e9013bf6185306f51096
PradeepRanjan18 <71320044+PradeepRanjan18@users.noreply.github.com>
2022-02-16 21:12:11 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,9 +1,9 @@
 variable "access_key" {
-  default = "AKIAVFBR3CHWJZY2S3UV"
+  default = "AKIARZDMKDTSGXISVG4R"
   }
 variable "secret_key" {
-  default = "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"
+  default = "i5+9m7gUsMwXaEjpS0OrTDGPfbQVzVs1ynj+OUN2"
   }
 variable "region" {
-  default = "ap-south-1"
+  default = "us-east-1"
   }
