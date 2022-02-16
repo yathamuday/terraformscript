a90af11e35056381bc7a41aa2cb322411f219616
PradeepRanjan18 <71320044+PradeepRanjan18@users.noreply.github.com>
2022-02-16 21:13:58 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,9 +1,9 @@
 resource "aws_s3_bucket" "bucklebuck" {
-  bucket = "my-tf-massiveprince-bucket11"
+  bucket = "my-tf-massiveprince-bucket1111"
   acl    = "private"
 
   tags = {
-    Name        = "rav1i-day1a-santhos1h-deepak1231"
+    Name        = "ttf-bucket"
     Environment = "Dev"
   }
 }
